f74af3b7625a959ddc1132a8975a6826a6571932
Ana Arias-Gonzalez <ana.ariasgonz@gmail.com>
2018-11-15 19:26:43 +0000

    Done.

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -56,18 +56,12 @@ def all_winter_holiday_supplies(holiday_hash)
   end
 
 def all_supplies_in_holidays(holiday_hash)
-  # iterate through holiday_hash and print items such that your readout resembles:
-  # Winter:
-  #   Christmas: Lights, Wreath
-  #   New Years: Party Hats
-  # Summer:
-  #   Fourth Of July: Fireworks, BBQ
-  # etc.
+
   holiday_hash.each do |season, holidays|
     puts "#{season.capitalize}:"
     holidays.each do |holiday, supplies|
-      binding.pry
-        holiday = holiday.capitalize.to_s
+      #binding.pry
+        holiday = holiday.capitalize.to_s.split("_").join(" ")
         supplies = supplies.join(", ")
         line = "  #{holiday}: #{supplies}"
         puts line
